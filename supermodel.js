a45518122f8ff940e867bbaf92e0e15b1db09797
Rich Harris <richard.a.harris@gmail.com>
2012-11-29 14:19:01 +0000

    refreshed docs

--- supermodel.js
+++ supermodel.js
@@ -1,3 +1,5 @@
+/*global module, define */
+
 (function ( global ) {
 
 	'use strict';
@@ -109,6 +111,8 @@
 			if ( !silent && ( force || !isEqual( previous, value ) ) ) {
 				this._notifyObservers( keypath, value, previous, force );
 			}
+
+			return this;
 		},
 
 		// Get item from our model. Again, can be arbitrarily deep, e.g.
@@ -203,6 +207,17 @@
 			return returnedObservers;
 		},
 
+		observeOnce: function ( keypath, callback ) {
+			var self = this, suicidalObserver;
+
+			suicidalObserver = this.observe( keypath, function ( value, previousValue ) {
+				callback( value, previousValue );
+				self.unobserve( suicidalObserver );
+			});
+
+			return this;
+		},
+
 		// Cancel observer(s)
 		unobserve: function ( observerToCancel ) {
 			var observers, index, keypath;
@@ -237,6 +252,8 @@
 			if ( observers.length === 0 ) {
 				delete this._observers[ keypath ];
 			}
+
+			return this;
 		},
 
 		// Internal publish method
@@ -314,128 +331,22 @@
 		return splitKeypath( keypath ).join( '.' );
 	};
 
-	// borrowed wholesale from underscore... TODO include license? write a Supermodel-optimised version?
 	isEqual = function ( a, b ) {
 		
-		var eq = function ( a, b, stack ) {
+		// workaround for null...
+		if ( a === null && b === null ) {
+			return true;
+		}
 
-			var toString, className, length, size, result, key;
+		// If a or b is an object, return false. Otherwise `set( key, value )` will fail to notify
+		// observers of `key` if `value` is the same object or array as it was before, even though
+		// the contents of changed
+		if ( typeof a === 'object' || typeof b === 'object' ) {
+			return false;
+		}
 
-			toString = Object.prototype.toString;
-			
-			// Identical objects are equal. `0 === -0`, but they aren't identical.
-			// See the Harmony `egal` proposal: http://wiki.ecmascript.org/doku.php?id=harmony:egal.
-			if ( a === b ) {
-				return ( a !== 0 || ( 1 / a == 1 / b ) );
-			}
-			
-			// A strict comparison is necessary because `null == undefined`.
-			if ( a == null || b == null ) {
-				return a === b;
-			}
-			
-			// Compare `[[Class]]` names.
-			className = toString.call( a );
-			if ( className != toString.call( b ) ) {
-				return false;
-			}
-			
-			switch ( className ) {
-				// Strings, numbers, dates, and booleans are compared by value.
-				case '[object String]':
-					// Primitives and their corresponding object wrappers are equivalent; thus, `"5"` is
-					// equivalent to `new String("5")`.
-					return a == String( b );
-				
-				case '[object Number]':
-					// `NaN`s are equivalent, but non-reflexive. An `egal` comparison is performed for
-					// other numeric values.
-					return ( ( a != +a ) ? ( b != +b ) : ( a == 0 ? ( 1 / a == 1 / b ) : ( a == +b ) ) );
-				
-				case '[object Date]':
-				case '[object Boolean]':
-					// Coerce dates and booleans to numeric primitive values. Dates are compared by their
-					// millisecond representations. Note that invalid dates with millisecond representations
-					// of `NaN` are not equivalent.
-					return +a == +b;
-				// RegExps are compared by their source patterns and flags.
-				case '[object RegExp]':
-					return a.source == b.source &&
-						a.global == b.global &&
-						a.multiline == b.multiline &&
-						a.ignoreCase == b.ignoreCase;
-			}
-
-			if ( typeof a != 'object' || typeof b != 'object' ) {
-				return false;
-			}
-			
-			// Assume equality for cyclic structures. The algorithm for detecting cyclic
-			// structures is adapted from ES 5.1 section 15.12.3, abstract operation `JO`.
-			length = stack.length;
-			
-			while ( length-- ) {
-				// Linear search. Performance is inversely proportional to the number of
-				// unique nested structures.
-				if ( stack[length] == a ) {
-					return true;
-				}
-			}
-			
-			// Add the first object to the stack of traversed objects.
-			stack.push( a );
-
-			size = 0, result = true;
-			// Recursively compare objects and arrays.
-			
-			if ( className == '[object Array]' ) {
-				
-				// Compare array lengths to determine if a deep comparison is necessary.
-				size = a.length;
-				result = size == b.length;
-				
-				if ( result ) {
-					// Deep compare the contents, ignoring non-numeric properties.
-					while ( size-- ) {
-					// Ensure commutative equality for sparse arrays.
-						if ( !( result = size in a == size in b && eq( a[ size ], b[ size ], stack ) ) ) {
-							break;
-						}
-					}
-				}
-			} else {
-				// Objects with different constructors are not equivalent.
-				if ( 'constructor' in a != 'constructor' in b || a.constructor != b.constructor ) {
-					return false;
-				}
-				
-				// Deep compare objects.
-				for ( key in a ) {
-					if ( a.hasOwnProperty( key ) ) {
-						// Count the expected number of properties.
-						size++;
-						// Deep compare each member.
-						if ( !( result = b.hasOwnProperty( key ) && eq( a[ key ], b[ key ], stack ) ) ) {
-							break;
-						}
-					}
-				}
-
-				// Ensure that both objects contain the same number of properties.
-				if ( result ) {
-					for ( key in b ) {
-						if ( b.hasOwnProperty( key ) && !( size-- ) ) break;
-					}
-					result = !size;
-				}
-			}
-
-			// Remove the first object from the stack of traversed objects.
-			stack.pop();
-			return result;
-		};
-
-		return eq( a, b, [] );
+		// we're left with a primitive
+		return a === b;
 	};
 
 	
@@ -448,7 +359,7 @@
 	// AMD - define module
 	else if ( typeof define === 'function' && define.amd ) {
 		define( function () {
-			return Supermodel
+			return Supermodel;
 		});
 	}
 
